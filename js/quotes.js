6d4b99a0fb7b14f175cf7d1a6281d6dc25548c02
Marlon Pleitez <alpha@Marlons-MacBook-Pro.local>
2017-06-02 21:53:26 -0500

    meets expectations change

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,52 @@
+// serpate quotes js file to modify quotes easier
 var quotes = [
-    {quote: "", source: ""},
-    {quote: "", source: ""},
-    {quote: "", source: ""},
-    {quote: "", source: ""},
-    {quote: "", source: ""},
+    {quote: "You miss 100 percent of the shots you don't take.", 
+     source: "Wayne Gretzky", 
+     citation: "Michael Scott", 
+     year: 2009, 
+     category: "humor"
+    },
+    {quote: "One day of practice is like one day of clean living. It doesn't do you any good.", 
+     source: "Abe Lemmons", 
+     category: "sports"
+    },
+    {quote: "To succeed...You need to find something to hold on to, something to motivate you, something to inspire you.", 
+     source: "Tony Dorsett", 
+     category: "sports"
+    },
+    {quote: "Pain is temporary. It may last a minute, or an hour, or a day, or a year, but eventually it will subside and something else will take its place. If I quit, however, it lasts forever.", 
+     source: "Lance Armstrong", 
+     category: "sports"
+    },
+    {quote: "Treat a person as he is, and he will remain as he is. Treat him as he could be, and he will become what he should be.",    source: "Jimmy Johnson", 
+     category: "sports"
+    },
+    {quote: "Don't cry because it's over, smile because it happened.", 
+     source: "Dr. Suess", 
+     category: "inspirational", 
+     year: 1999
+    },
+    {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", 
+     source: "Albert Einstien", 
+     category: "humor"
+    },
+    {quote: "In three words I can sum up everything I've learned about life: it goes on", 
+     source: "Robert Frost", 
+     citation: "The Harper Book of Quotations",
+     category: "inspirational", 
+     year: 1993
+    },
+    {quote: "I'm not superstitious, but I am a little stitious.", 
+     source: "Michael Scott", 
+     category: "humor", 
+     year: 2007
+    },
+    {quote: "I have never taken the high road, but I tell other people to, ‘cause then there’s more room for me on the low road.", 
+     source: "Tom Haveford", 
+     citation: "Parks and Recreation",
+     category: "inspirational", 
+     year: 2009
+    }
 ];
+
+var quotesCopy = [];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,34 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//get random quote from quotes.js and return quote object
+function getRandomQuote() {
+    //returns random # between 0 and the length of the quotes array
+    var rand = Math.floor(Math.random() * quotes.length); 
+    return quotes[rand];
+}
+
+//print the quote from getRandomQuote()
+function printQuote() {
+    var quote = getRandomQuote();
+    document.getElementById('quote-box').innerHTML = format(quote);
+}
+
+//formats the quote to the correct html tags
+function format(quote) {
+    var message = "";
+    
+    message += "<p class=\"quote\">" + quote.quote + "</p>";
+    message += "<p class=\"source\">" + quote.source;
+    
+    //cond. statements check to see if citation and/or year exists, if it does it adds it to the message
+    if (quote.citation !== undefined){
+        message += "<span class=\"citation\">" + quote.citation + "</span>";
+    }
+    if (quote.year !== undefined){
+        message += "<span class=\"year\">" + quote.year + "</span>";
+    }
+    
+    message += "</p>";
+    return message;
+}
